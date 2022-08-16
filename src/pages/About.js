cef3389230c34b1de76e35191b56a273bc64e42e
Claire <63174119+clairemanthey@users.noreply.github.com>
2022-08-16 16:03:06 -0500

    Uploaded pages folder

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Sea breeze, macduff glenmorangie french 75 lochnagar leroux rusty nail. 
+        Grog horse’s neck finlandia colorado bulldog, tomintoul drumguish lime rickey mint 
+        julep culto a la vida. Glentauchers sundowner kensington court special: brandy alexander 
+        salmiakki koskenkorva calvert porto flip. Ardmore, “caribou lou nikolaschka stolichnaya, 
+        tonic and tequila leroux v.o., salmiakki koskenkorva.”
+      </p>
+    </section>
+  )
+}
+
+export default About
